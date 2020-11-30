8e19184a7c554bbfbb4cefa6cbae71c89e6bc229
jacopo <jacopotrapani89@gmail.com>
2020-11-30 13:20:15 +0100

    added endpoint customer and direct payment

--- REST/PaymentEndpoints.php
+++ REST/PaymentEndpoints.php
@@ -10,4 +10,14 @@ class PaymentEndpoints
     {
         return new Endpoint('api/create_payment', 'POST');
     }
+
+    static public function createCustomer(): Endpoint
+    {
+        return new Endpoint('api/customer/create-customer', 'POST');
+    }
+
+    static public function createDirectPayment(): Endpoint
+    {
+        return new Endpoint('api/payment/direct-payment', 'POST');
+    }
 }
